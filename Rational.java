5d73ee8c7403f5fff5efb50e77c097bcea53ea55
jimyuan42x <jimyuan42x@gmail.com>
2013-11-18 22:59:35 -0500

    Cannot figure out multiply
    I cannot get past phase 1

--- Rational.java
+++ Rational.java
@@ -17,8 +17,8 @@ public class Rational{
     }
 
        public Rational(int numer, int denom){
-	   System.out.println(numer);
-	   System.out.println(denom);
+	   //System.out.println(numer);
+	   // System.out.println(denom);
 
 	if (denom == 0){
 	    numerator = 0;
@@ -29,20 +29,20 @@ public class Rational{
 	    numerator = numer;
 	    denominator = denom;
 	}
-	System.out.println(numerator);
-	System.out.println(denominator);
+	   //	System.out.println(numerator);
+	   //	System.out.println(denominator);
 
     }
     
     public String toString(){
-	System.out.println(numerator);
-	System.out.println(denominator);
+	   //	System.out.println(numerator);
+	   //	System.out.println(denominator);
 	value = (double)numerator / (double)denominator;//now i feel stupid
-	System.out.println(value);
+	   //	System.out.println(value);
 	String ans = "";
-	System.out.println(ans);
+	   //	System.out.println(ans);
 	ans += msg;
-	System.out.println(ans);
+	   //	System.out.println(ans);
 	if (msg.equals("")){
 	    ans +=  value;
 
@@ -52,10 +52,18 @@ public class Rational{
 	    ans += "The value be here: " + value;	    
 
 	}
-	System.out.println(ans);
+	   //	System.out.println(ans);
 	    return ans;
     }
 
+
+    public static float floatValue(Rational num){
+	return (float)(num.value);
+    }
+
+    public static void multiply(Rational num){
+	
+    }
     public static void main(String []args){
 
 	//make a Rational
@@ -63,8 +71,14 @@ public class Rational{
 	System.out.println("This is the default value of my default Rational: " + num);
 	Rational numOV = new Rational(4, 0);
 	System.out.println("This is Rational(4,0): " + numOV);
+	System.out.println("Floating numbers~ should match above: " + floatValue(numOV));
 	Rational anotherNum = new Rational(3, 4);
 	System.out.println("This is Rational(3,4): " + anotherNum);
+	System.out.println("Floating numbers~ should match above: " + floatValue(anotherNum));
+	//try additional case b/c just got ^that thing up there to work
+	Rational number = new Rational(100, 2);
+	System.out.println("This is Rational(100,2): " + number);
+	System.out.println("Floating numbers~ should match above: " + floatValue(number));
 
     }
 
